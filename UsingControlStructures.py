6d1551996d961ec885784f7f267c32d54b396499
Dede Ankrah <pcspida@ubuntu.(none)>
2012-06-20 01:30:50 +0000

    controlstrings

---
+++ UsingControlStructures.py
@@ -0,0 +1,7 @@
+theInput = raw_input("Enter an integer:")
+#Your code here
+integer=int(theInput)
+if integer%2==0:
+    print integer, "is even"
+else:
+    print integer, "is odd"
---
+++ userinput.py
@@ -0,0 +1,7 @@
+first_name = raw_input('Enter your first name')
+last_name=raw_input('Enter your last name:')
+print 'Enter your date of birth:'
+month=raw_input('Month?')
+day=raw_input('Day?')
+yr=raw_input('Year?')
+print first_name,last_name,"was born on",month,day,yr
